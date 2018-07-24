f29f2d4885a2753d5f6440398ef2a83d8249b442
David <throwasix@gmail.com>
2018-07-24 16:47:31 +0100

    Timeout overridden in async auth test

--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -3,15 +3,20 @@ var authController = require('../../controllers/authController');
 
 describe('AuthController', function() {
     describe('isAuthorized', function() {
-        it('Should return false if not authorized', function() {
+        // Mocha suggests NOT using arrow function due to binding of this
+        it('Should return false if not authorized', () => {
             assert.equal(false, authController.isAuthorized(['user'], 'admin'));
         });
-        it('Should return true if not authorized', function() {
+        it('Should return true if not authorized', () => {
             assert.equal(true, authController.isAuthorized(['user', 'admin'], 'admin'));
         });
     });
     describe('isAuthorizedAsync', function() {
+        // Not using arrow functions as it would bind this to the lexical
+        // rather than the callpath which woould preventt us from 
+        // overriding the timeout limit (2000)
         it('Should return false if not authorized', function(done) {
+            this.timeout(2500);
             authController.isAuthorizedAsync(['user'], 'admin', 
             function(isAuth) {
                 assert.equal(false, isAuth);
