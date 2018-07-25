fd71fad885af58fe952dbcdef9f6ce84117a580b
David <throwasix@gmail.com>
2018-07-25 11:30:35 +0100

    Using Should over Expect

--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var authController = require('../../controllers/authController');
 var expect = require('chai').expect;
+var should = require('chai').should();
 
 describe('AuthController', function() {
     beforeEach(function settingUpRoles() {
@@ -10,24 +11,24 @@ describe('AuthController', function() {
         // Mocha suggests NOT using arrow function due to binding of this
         it('Should return false if not authorized', () => {
             var isAuth = authController.isAuthorized('admin');
-            expect(isAuth).to.be.false;
+            isAuth.should.be.false;
         });
         it('Should return true if not authorized', () => {
             authController.setRoles(['user', 'admin']);
             var isAuth = authController.isAuthorized('admin');
-            expect(isAuth).to.be.true;
+            isAuth.should.be.true;
         });
         it('Should not allow a get if not authorised');
         it('should allow get if authorized');
     });
     describe('isAuthorizedAsync', function() {
         // Not using arrow functions as it would bind this to the lexical
-        // rather than the callpath which woould preventt us from 
+        // rather than the callpath which would prevent us from 
         // overriding the timeout limit (2000)
         it('Should return false if not authorized', function(done) {
             authController.isAuthorizedAsync('admin', 
             function(isAuth) {
-                expect(isAuth).to.be.false;
+                isAuth.should.be.false;
                 done();
             });
         });
